4784481be842059f9809ceb3c7942f148dfd31b6
Aaron Azlant <azlant@gmail.com>
2015-12-16 21:13:49 -0800

    (fix): vendored HTML inputs, updated DB file to throw correct warnings / errors, updated logger-test for accurate linting

--- src/ParsedDataRepository/DB.js
+++ src/ParsedDataRepository/DB.js
@@ -1,3 +1,5 @@
+// #TODO: TEST
+
 import ParsedDataRepository from '../ParsedDataRepository';
 
 
@@ -9,6 +11,7 @@ class DB extends ParsedDataRepository {
     }
 
     setRootPath(rootPath) {
+        throw new Error('Not implemented.');
     }
 
     async saveParsedShows(parsedShows) {
@@ -36,42 +39,43 @@ class DB extends ParsedDataRepository {
                 let show;
                 let join;
 
-                try { 
+                try {
                     artist = await this.db.one('INSERT INTO artist (name) VALUES (${name}) RETURNING id', { name: parsedShow.band });
                     this.logger.info(`saved artist "${parsedShow.band}" to db as ID#:${artist.id}`);
                 } catch (error) {
                     if (error.message.includes('duplicate key value violates')) {
                         artist = await this.db.one('SELECT id FROM artist WHERE name = ${name}', { name: parsedShow.band });
-                        this.logger.warn(`duplicate artist "${parsedShow.band}" to db as ID#:${artist.id}`);
+                        this.logger.warn(`duplicate artist "${parsedShow.band}" already in db as ID#:${artist.id}`);
                     }
                 }
 
-                try { 
+                try {
                     show = await this.db.one('INSERT INTO shows (venue, price, is_sold, ages, date) VALUES (${venue}, ${price}, ${soldOut}, ${ages}, ${date}) RETURNING id', { venue: parsedShow.venue, price: parsedShow.price, soldOut: parsedShow.soldOut, ages: parsedShow.ages, date: date });
-                    this.logger.info(`saved show for artist "${parsedShow.band}" on ${date} at "${parsedShow.venue}" to db as ID#:${show.id}`);
+                    this.logger.info(`saved show on ${date} at "${parsedShow.venue}" to db as ID#:${show.id}`);
                 } catch (error) {
                     if (error.message.includes('duplicate key value violates')) {
                         show = await this.db.one('SELECT id FROM shows WHERE venue = ${venue} AND date = ${date}', { venue: parsedShow.venue, date: date });
+                        this.logger.warn(`duplicate show on ${date} at "${parsedShow.venue}" already in db as ID#:${show.id}`);   
                     }
                 }
 
-                try { 
+                try {
                     join = await this.db.one('INSERT INTO show_artist (show_id, artist_id) VALUES (${show_id}, ${artist_id}) RETURNING id', { show_id: show.id, artist_id: artist.id});
                     this.logger.info(`saved join show ID#:${show.id} and artist ID#:${artist.id} as ID#:${join.id}`);
                 } catch (error) {
                     if (error.message.includes('duplicate key value violates')) {
                         join = await this.db.one('SELECT id FROM show_artist WHERE show_id = ${show_id} AND artist_id = ${artist_id}', { show_id: show.id, artist_id: artist.id});
+                        this.logger.warn(`duplicate join between show ID#:${show.id} and artist ID#:${artist.id} already in db as ID#:${join.id}`);
                     }
                 }
             }
-
         } catch (error) {            
             this.logger.error(error);
         }
     }
 
     fetchedParsedShows() {
-    
+        throw new Error('Not implemented.');
     }
 
 }
--- test/logger-test.js
+++ test/logger-test.js
@@ -4,26 +4,24 @@ import sinon from 'sinon';
 import Logger from '../src/Logger';
 
 describe('Logger', ()=> {
-
     it('should call self.log(msg, error) on error()', () => {        
-        const logger = new Logger()
+        const logger = new Logger();
         const mockedLog = sinon.stub(logger, 'log');
-        logger.error("test message");
-        expect(mockedLog.calledWith("test message", "error")).to.be.ok;
+        logger.error('test message');
+        expect(mockedLog.calledWith('test message', 'error')).to.be.ok; // eslint-disable-line no-unused-expressions
     });
 
     it('should call self.log(msg, warn) on warn()', () => {
-        const logger = new Logger()
+        const logger = new Logger();
         const mockedLog = sinon.stub(logger, 'log');
-        logger.warn("test message");
-        expect(mockedLog.calledWith("test message", "warn")).to.be.ok;
+        logger.warn('test message');
+        expect(mockedLog.calledWith('test message', 'warn')).to.be.ok;// eslint-disable-line no-unused-expressions
     });
 
     it('should call self.log(msg, info) on info()', () => {
-        const logger = new Logger()
+        const logger = new Logger();
         const mockedLog = sinon.stub(logger, 'log');
-        logger.info("test message");
-        expect(mockedLog.calledWith("test message", "info")).to.be.ok;
+        logger.info('test message');
+        expect(mockedLog.calledWith('test message', 'info')).to.be.ok;// eslint-disable-line no-unused-expressions
     });
-
 });
