80e339d2e6264b96f4f0b4660f0f1fddbe1ef1c2
Aaron Azlant <azlant@gmail.com>
2016-01-09 00:31:43 -0800

    (feat): showsList component now prepares data and renders a shows by date though it is janky.

--- src/client/components/ShowsList.js
+++ src/client/components/ShowsList.js
@@ -1,19 +1,82 @@
 import React, { PropTypes, Component } from 'react';
 import moment from 'moment';
 
+const buildShowItemsByDate = (items)=> {
+    const showsByDate = {};
+
+    for (const item in items) {
+        if (items.hasOwnProperty(item)) {
+            const { id, date, bands, venue, time, soldOut, pit, multiDay, ages, price } = items[item];
+            const showContent = {
+                id,
+                bands,
+                venue,
+                time,
+                soldOut,
+                pit,
+                multiDay,
+                ages,
+                price,
+            };
+
+            if (date in showsByDate) {
+                showsByDate[date].push(showContent);
+            } else {
+                showsByDate[date] = [showContent];
+            }
+        }
+    }
+
+    const shows = [];
+
+    for (const item in showsByDate) {
+        if (showsByDate.hasOwnProperty(item)) {
+            const show = {
+                date: item,
+                shows: showsByDate[item],
+            };
+
+            shows.push(show);
+        }
+    }
+
+    return shows;
+};
+
+const showsStyle = {marginBottom: '1em'};
+const showStyle = {marginLeft: '1em', marginTop: '1em', marginBottom: '1em'};
+const showItem = {marginRight: '2em', marginTop: '.5em'};
+const bandStyle = {marginLeft: '4em', fontStyle: 'italic'};
+
 export default class ShowsList extends Component {
   render() {
+      const items = buildShowItemsByDate(this.props.items);
+
       return (
         <ul>
-          {this.props.items.map((item) =>
-            <li key={item.id}>
-              <p>{moment(item.date).format('MMMM Do, YYYY')}</p>
-              <p>{item.venue}</p>
-              {item.bands.map((band, i)=>
-                <div key={i}><b>{band}</b></div>
-              )}
-            </li>
-          )}
+          {items.map((item)=>{
+              // #TODO: move below to subcomponents
+              return (<li>
+                <b>{moment(item.date).format('MMMM Do, YYYY')}</b>
+                <div className="shows" style={showsStyle}>
+                  {item.shows.map((show)=>
+                      <div key={show.id}>
+                        <div className="show" style={showStyle}>
+                          <span className="venue" style={showItem}>{show.venue}</span>
+                          <span className="time" style={showItem}>{show.time}</span>
+                          <span className="price" style={showItem}>{show.price}</span>
+                        </div>
+                        <div className="bands">
+                          {show.bands.map((band, i)=>
+                            <div className="band" style={bandStyle} key={i}>{band}</div>
+                          )}
+                        </div>
+                      </div>
+
+                  )}
+                </div>
+              </li>);
+          })}
         </ul>
       );
   }
