b200a1c91039f8cba5e2eecda94e11f1d0731d72
Aaron Azlant <azlant@gmail.com>
2016-02-12 00:41:11 -0800

    (feat): moved google auth routes into /services/auth/google

--- src/server/api/routes.js
+++ src/server/api/routes.js
@@ -1,6 +1,4 @@
-import { Types } from 'hapi';
 import postgres from 'pg-promise';
-import crypto from 'crypto';
 import config from '../config';
 
 import * as google from './services/auth/google';
@@ -38,44 +36,7 @@ export const showsRoute = {
     },
 };
 
-export const googleAuthRoute = {
-    method: 'POST',
-    path: '/auth/google',
-    config: {
-        handler: async (request, reply)=> {
-            const { payload: { accessToken } } = request;
-            try {
-                await google.validateToken(accessToken);
-                const {id, email, name} = await google.getUserInfo(accessToken);
-                // #TODO: check if user already exists
-                // #TODO: create user in DB
-                // #TODO: save token to DB - learn about worker
-            } catch (error) {
-                // #TODO: use stronger errors library, http://npmjs.com/package/errors
-                console.error(error);
-            }
-
-        },
-    },
-};
-
-export const googleRedirect = {
-    method: 'GET',
-    path: '/auth/google/redirect',
-    config: {
-        handler: (request, reply)=> {
-            const nonce = crypto.randomBytes(48).toString('hex');
-            const redirectUri = `http://${config.appHost}:${config.appPort}/auth/google/callback`;
-            const scope = `https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`;
-            const baseUrl = `https://accounts.google.com/o/oauth2/v2/auth`;
-            const query = `?response_type=token&state=${nonce}&redirect_uri=${redirectUri}&scope=${scope}&client_id=${config.googleClientId}`;
-            reply.redirect(`${baseUrl}${query}`);
-        },
-    },
-};
-
 export default [
     showsRoute,
-    googleAuthRoute,
-    googleRedirect,
+    ...google.authRoutes,
 ];
